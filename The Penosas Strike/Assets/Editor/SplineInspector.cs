f5a07e07cdef33c30e458008254b7aba98281dcf
LucasZaranza <lucaszaranza@gmail.com>
2018-01-24 16:35:43 -0300

    Environment setup to .NET 4.6

---
+++ The Penosas Strike/Assets/Editor/SplineInspector.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Spline))]
+public class SplineInspector : Editor 
+{
+    
+}
---
+++ The Penosas Strike/Assets/Scripts/Spline.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Spline : MonoBehaviour 
+{
+	public Vector2 p0, p1;
+
+	void Start()
+	{
+        GameObject aloha;
+        Zaza();
+    }
+
+    public void Zaza() => Debug.Log("Uhuu!");
+}
